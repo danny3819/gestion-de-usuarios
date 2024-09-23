13542f9b7992adc33e50d2c0be6fafb076bf95ae
danny3819 <danny.nada.djnnr@gmail.com>
2024-09-23 16:07:26 -0600

    agregar tabla

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function doGet() {
   return HtmlService.createTemplateFromFile("index").evaluate().setTitle("gestion de usuarios");
 }
+
+function obtenerUsuarios(){
+    let hoja = SpreadsheetApp.openById("1b7elak8Q0SBfJn9-MTdnCgNm4WG2nwTBMns4oWqTrxo").getSheetByName("users");
+    let datos = hoja.getDataRange().getValues();
+    return datos
+}
