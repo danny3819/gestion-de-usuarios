99588f69aabe1687b69b9c02d3c94daadbdf3913
danny3819 <danny.nada.djnnr@gmail.com>
2024-09-23 19:23:01 -0600

    registrar usuario con modal

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+const hoja = SpreadsheetApp.openById("1b7elak8Q0SBfJn9-MTdnCgNm4WG2nwTBMns4oWqTrxo").getSheetByName("users");
+
 function doGet() {
-  return HtmlService.createTemplateFromFile("index").evaluate().setTitle("gestion de usuarios");
+    return HtmlService.createTemplateFromFile("index").evaluate().setTitle("gestion de usuarios");
 }
 
-function obtenerUsuarios(){
-    let hoja = SpreadsheetApp.openById("1b7elak8Q0SBfJn9-MTdnCgNm4WG2nwTBMns4oWqTrxo").getSheetByName("users");
-    let datos = hoja.getDataRange().getValues();
-    return datos
+// function doPost(datosResividos) {
+//     insertarUsuario(datosResividos.parameter.correo,
+//                      datosResividos.parameter.nombre,
+//                      "USER",
+//                      datosResividos.parameter.turno,
+//                      datosResividos.parameter.contrasena,)
+
+//     return HtmlService.createTemplateFromFile("index").evaluate().setTitle("gestion de usuarios");
+// }
+
+function obtenerUsuarios() {
+    let datos = hoja.getDataRange().getValues()
+    return datos;
+}
+
+function insertarUsuariogs(correo, nombre, tipo, turno, contrasena) {
+    hoja.appendRow([correo, nombre, tipo, turno, contrasena])
 }
